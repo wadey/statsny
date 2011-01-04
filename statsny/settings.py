a321798e10723f4f800569b3dd1eba04e5d4e8b2
Wade Simmons <wade@wades.im>
2011-01-04 14:18:07 -0700

    check STATSNY_CONFIG env variable

--- statsny/settings.py
+++ statsny/settings.py
@@ -1,8 +1,10 @@
+import os
+
 UDP_PORT  = 9026
 HTTP_PORT = 8026
 
 # Machine-specific config
 try:
-    execfile("/etc/statsny/settings.py")
+    execfile(os.environ.get("STATSNY_CONFIG", "/etc/statsny/settings.py"))
 except IOError:
     pass # No config
