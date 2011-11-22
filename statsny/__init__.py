c154a8817170ccca6a2105bf5438cd73b4173839
Jeremy Grosser <jeremy@synack.me>
2011-11-22 12:40:17 -0800

    Use simplejson if it's available

--- statsny/__init__.py
+++ statsny/__init__.py
@@ -1,4 +1,7 @@
-import json
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 from twisted.internet.protocol import DatagramProtocol, ServerFactory
 from twisted.web import server, resource
