9462a2ef89820c40ec1bcd469fbd187a889fd2ff
Wade Simmons <wade@wades.im>
2013-06-06 13:54:35 -0700

    cleanup

--- debian/settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# © 2010 SimpleGeo, Inc. All rights reserved.
-#
-
-"""Local settings for Statsny.
-
-Any value set here will override the default settings in the package.
-"""
